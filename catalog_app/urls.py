3f750a8d94a7a8df6812479447041bffc399b7a4
duckit69 <gherab.radouane@gmail.com>
2026-08-02 00:25:06 +0100

    added the search logic

--- catalog_app/urls.py
+++ catalog_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('products', views.product_list, name='product_list'),
+    path('products/search', views.product_search, name='product_search'),
     path('products/<slug:slug>', views.product_detail, name='product_detail'),
     path('products/<int:product_id>/reviews/', views.review_manager, name='add_review'),
     path('categories', views.category_list, name='category_list'),
--- catalog_app/views.py
+++ catalog_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models import Q
 # Create your views here.
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -65,3 +65,15 @@ def review_manager(request, product_id):
     return Response(serializer.data)
 
 
+@api_view(['GET'])
+def product_search(request):
+    query = request.query_params.get('query')
+
+    # maybe it does not make sense to search category and product name 
+    # just testing Q object and playing with | & ~
+    products = Product.objects.filter(Q(name__icontains=query)
+                                      | Q(description__icontains=query)
+                                      | Q(category__name__name=query))
+
+    serializer = ProductListSerializer(products, many=True)
+    return Response(serializer.data)
